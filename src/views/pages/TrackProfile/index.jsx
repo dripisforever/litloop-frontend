952747a364ec37a6276472a3976283de96e97e1a
unknown <driptamine@gmail.com>
2023-10-16 16:42:09 +0600

    Added 6th template video

--- src/views/pages/TrackProfile/index.jsx
+++ src/views/pages/TrackProfile/index.jsx
@@ -99,6 +99,19 @@ const api_data = [
   },
   {
     // url: "https://d1ca20q97pi6ei.cloudfront.net/Crystal+Castles+-+Kerosene(American+Psycho).mp4",
+    url: "https://dm0qx8t0i9gc9.cloudfront.net/watermarks/video/r_Mvoq7Oliyrkej0t/videoblocks-v1-0002_dji_0748_r47ojmzkq__ae26123a57af1f09e97017a982c56a24__P360.mp4",
+    viewsCount: "3.2M",
+    likesCount: "100K",
+    // thumbNail: "https://d1ca20q97pi6ei.cloudfront.net/thumbnail/Crystal+Castles+-+Kerosene(American+Psycho)+.jpeg",
+
+    // thumbNail: "https%3A%2F%2Fd1ca20q97pi6ei.cloudfront.net%2Fthumbnail%2FCrystal%2BCastles%2B-%2BKerosene(American%2BPsycho)%2B.jpeg",
+    thumbNail: "https://images.unsplash.com/photo-1560440293-855922f9cc7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+
+
+    // thumbNail: "https://views-test-api.s3.us-west-1.amazonaws.com/thumbnail/Crystal+Castles+-+Kerosene(American+Psycho)+.jpeg",
+  },
+  {
+    // url: "https://d1ca20q97pi6ei.cloudfront.net/Crystal+Castles+-+Kerosene(American+Psycho).mp4",
     url: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa_video_1080_4800000.m3u8",
     viewsCount: "3.2M",
     likesCount: "100K",
