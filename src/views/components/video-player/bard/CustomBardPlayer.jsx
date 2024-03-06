7844033d554049bbb29c1ed35bff6dd8421fecda
dripset <driptamin@gmail.com>
2024-03-06 23:56:47 +0600

    bard hls player

--- src/views/components/video-player/bard/CustomBardPlayer.jsx
+++ src/views/components/video-player/bard/CustomBardPlayer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HlsPlayerV2 from './HlsPlayerV2';
+import HlsPlayerV2 from 'views/components/video-player/bard/HLSPlayerV2.jsx';
 
 const CustomBardPlayer = ({url, }) => {
   const mp4Source = 'https://cdn.coverr.co/videos/coverr-golden-dunes-4546/1080p.mp4';
